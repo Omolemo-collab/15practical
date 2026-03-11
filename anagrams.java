acca58524b5991540d308ede854f2ffdd8d40434
Omolemo_Modise <omolemo.modise.mogudi@gmail.com>
2026-03-11 12:25:58 +0000

    signature mathod created

--- anagrams.java
+++ anagrams.java
@@ -1,1 +1,14 @@
 //start anagrams project
+import java.util.Arrays;
+
+pubilc class anagrams {
+    // create signature method to return a string with the characters in sorted order
+    public static String signature(String s){
+        char [] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars); 
+    }
+    public static void main(String[] args) throws Exception {
+
+    }
+}
