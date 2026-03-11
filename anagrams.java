4231aea7b71cb6c48c20565f609eefa101a34cab
Omolemo_Modise <omolemo.modise.mogudi@gmail.com>
2026-03-11 12:48:12 +0000

    added reader and writer method

--- anagrams.java
+++ anagrams.java
@@ -10,5 +10,33 @@ pubilc class anagrams {
     }
     public static void main(String[] args) throws Exception {
 
+        BuffererReader reader = new BuffererReader(new FileReader("ulysses.text"));
+        String line; 
+        while ((line = reader.readline()) != null){
+
+           String[] words = line.split("\\s+");
+
+            for (String word : words){
+                String sig = signature(word);
+
+                if(!groups.containsKey (sig)){
+                    groups.put(sig, new ArrayList<String>());
+                }
+
+                groups.get(sig).add(word);
+            }
+
+            Bufferedwriter writer = new Bufferedwriter(new Filewriter("theAnagrams.text"))
+            writer.write("anagrams in the text: \n");
+            for (List<String> anagramGroup : groups.values()){
+                if (anagramGroup.size() > 1){
+                    writer.write(anagramGroup.toString() + "\n");
+                }
+            }
+            writer.close();
+
+            reader.close();
+
+        }
     }
 }
